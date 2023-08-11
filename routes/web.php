e1fcffe100563e045adcf0c0d3310685ededccd4
yasu-7 <yasuweiyou542@gmail.com>
2023-08-11 20:36:11 +0900

    Add 初期設定＆基礎実装

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,8 @@ use APP\Http\Controllers\PostController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return view('welcome'); }
+);
 
 Route::get('/posts',[PostControlller::class, 'index']);
