513e2f2ba12a4ec680eef3fda6ce614c8de5bdc4
yasu-7 <yasuweiyou542@gmail.com>
2023-08-11 22:01:31 +0900

    Add 初期設定＆基礎実装

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
     return view('welcome'); }
 );
 
-Route::get('/posts',[PostControlller::class, 'index']);
+Route::get('/posts', [PostControlller::class, 'index']);
