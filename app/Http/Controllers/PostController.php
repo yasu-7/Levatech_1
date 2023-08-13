60b56795eea9947268525cae2f06f6d17fff5c50
yasu-7 <yasuweiyou542@gmail.com>
2023-08-13 16:47:20 +0900

    ADD　ブログの編集削除

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -40,6 +40,13 @@ class PostController extends Controller
         $input_post = $request['post'];
         $post->fill($input_post)->save();
         return redirect('/posts/' . $post->id);
+        
+    }
+    
+    public function delete(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
     }
 }
 ?>
